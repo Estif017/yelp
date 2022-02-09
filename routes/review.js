9e2fe6ceec35792b1cf5fd9b83c2985079e7e8af
Estifanos Teklearegay <estifoteklearegay@gmail.com>
2022-02-08 19:14:15 -0800

    added authorization on campground and reviews to only a user that creates campground/reviews to make a change

--- routes/review.js
+++ routes/review.js
@@ -3,15 +3,17 @@ const router = express.Router({ mergeParams: true });
 const Campground = require('../models/campground');
 const Review = require('../models/reviews');
 const catchAsync = require('../utils/catchAsync');
-const validateReview = require('../utils/validateReviews');
+const { validateReview, isLoggedIn, isReviewAuthor } = require('../middleware');
 
 router.post(
 	'/',
 	validateReview,
+	isLoggedIn,
 	catchAsync(async (req, res) => {
 		const campground = await Campground.findById(req.params.id);
 		const review = new Review(req.body.review);
 		await campground.reviews.push(review);
+		review.author = req.user._id;
 		await review.save();
 		await campground.save();
 		req.flash('success', 'Successfully Made a new review!');
@@ -21,6 +23,8 @@ router.post(
 
 router.delete(
 	'/:reviewId',
+	isLoggedIn,
+	isReviewAuthor,
 	catchAsync(async (req, res) => {
 		const { id, reviewId } = req.params;
 		await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
