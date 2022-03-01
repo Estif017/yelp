ae2313468d2831ed5a23c210637753f6e88622d7
Estifanos Teklearegay <estifoteklearegay@gmail.com>
2022-03-01 08:08:02 -0800

    managed to store uploaded photos to mongo db and display on the show router

--- routes/campground.js
+++ routes/campground.js
@@ -1,23 +1,18 @@
 const express = require('express');
+const multer = require('multer');
 const router = express.Router();
 const catchAsync = require('../utils/catchAsync');
 const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
 const campgrounds = require('../controllers/campground');
-const multer = require('multer');
-const upload = multer({ dest: 'uploads/' });
+const { storage } = require('../cloudinary');
+const upload = multer({ storage });
 
-router
-	.route('/')
-	.get(catchAsync(campgrounds.allCampgrounds))
-	// .post(
-	// 	isLoggedIn,
-	// 	validateCampground,
-	// 	catchAsync(campgrounds.createNewCampground)
-	// );
-	.post(upload.array('image'), (req, res) => {
-		console.log(req.files);
-		res.send(req.body);
-	});
+router.route('/').get(catchAsync(campgrounds.allCampgrounds)).post(
+	isLoggedIn,
+	upload.array('image'),
+	// validateCampground,
+	catchAsync(campgrounds.createNewCampground)
+);
 
 router.get('/new', isLoggedIn, campgrounds.renderNewCampgroundForm);
 
